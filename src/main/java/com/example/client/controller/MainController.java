5b49ec611c0a232b61bd36e7b02ab7d7c6f2dc5c
juan <crumbled23@gmail.com>
2024-07-08 17:24:36 +0900

    eureka client

--- src/main/java/com/example/client/controller/MainController.java
+++ src/main/java/com/example/client/controller/MainController.java
@@ -3,6 +3,8 @@ package com.example.client.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+
 @Controller
 public class MainController {
     @GetMapping("/")
